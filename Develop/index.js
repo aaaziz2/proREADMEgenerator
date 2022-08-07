803147a96d491c7b7f1ffd0eaf65f6a1496c016a
aaaziz2 <aumio.aziz@gmail.com>
2022-08-07 15:30:09 -0500

    added basic questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 'Title of the Project','Short Description',
+                    'Installation instructions','Usage information',
+                    'contribution guidelines', 'test instructions'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
