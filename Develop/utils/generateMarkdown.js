7e685768012e6572cb29226a9613ce4eefc6f70f
aaaziz2 <aumio.aziz@gmail.com>
2022-08-07 17:55:39 -0500

    Added info for different license still need to find badge and link info

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,38 @@
+const licenseInfo = [
+    {MIT, name:'MIT License', badge: '', link: ''},
+    {GNU, name:'GNU GPLv3 License',badge: '', link: ''},
+    {ISC, name:'ISC License', badge:'', link:''},
+    {APA, name:'Apache License 2.0', badge:'', link:''}
+]
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license){
+    return license.badge
+  }
+  else{
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license){
+    return license.link
+  }
+  else{
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license){
-    return license
+    return license.name
   }
   else{
     return ''
@@ -44,6 +66,7 @@ function generateMarkdown(data) {
   ## License
   
   ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
 
   ## How to Contribute
 
